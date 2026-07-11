swedishfrenchpress/cashu.me-swift

Unify amount-entry keypad footer spacing on Android
The Cashu Request amount sheet used an 8dp keypad-to-Done gap while the
other three amount-entry screens used 28dp, making it look cramped.
Extract the shared NumberPadFooter component so keypad + button +
nav-bar inset spacing can't drift apart per-screen again.

Co-Authored-By: Claude Sonnet 5 <noreply@anthropic.com>

android/app/src/main/java/org/cashu/wallet/ui/components/NumberPad.kt
@@ -10,8 +10,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Backspace
 import androidx.compose.material3.Icon
@@ -29,6 +33,7 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import org.cashu.wallet.Core.UnitAmountEntry
+import org.cashu.wallet.ui.theme.CashuTheme
 
 // Minimal keypad: no background boxes, just numbers with subtle press feedback.
 // Tighter vertical spacing for a cleaner iOS-style appearance.
@@ -100,6 +105,36 @@ fun NumberPad(
                 }
             }
         }
+    }
+}
+
+/**
+ * Shared tail for every amount-entry screen: [NumberPad] followed by a primary
+ * action button, spaced with the app-wide keypad→button gap (`spacing.page`)
+ * and a bottom spacer sized to the real navigation-bar/gesture-bar inset.
+ * Centralizing this stops each screen from hand-deriving its own spacing.
+ */
+@Composable
+fun NumberPadFooter(
+    amount: String,
+    onAmountChange: (String) -> Unit,
+    buttonText: String,
+    onButtonClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    decimals: Int = 0,
+    buttonEnabled: Boolean = true,
+    buttonLoading: Boolean = false,
+) {
+    Column(modifier = modifier) {
+        NumberPad(amount = amount, onAmountChange = onAmountChange, decimals = decimals)
+        Spacer(Modifier.height(CashuTheme.spacing.page))
+        PrimaryButton(
+            text = buttonText,
+            onClick = onButtonClick,
+            enabled = buttonEnabled,
+            loading = buttonLoading,
+        )
+        Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.navigationBars))
     }
 }
 

android/app/src/main/java/org/cashu/wallet/ui/receive/CashuRequestDetailScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -81,10 +80,9 @@ import org.cashu.wallet.ui.components.GhostButton
 import org.cashu.wallet.ui.components.InlineNoticeHost
 import org.cashu.wallet.ui.components.InspectorRow
 import org.cashu.wallet.ui.components.MintPickerSheet
-import org.cashu.wallet.ui.components.NumberPad
+import org.cashu.wallet.ui.components.NumberPadFooter
 import org.cashu.wallet.ui.components.PaymentStatusPhase
 import org.cashu.wallet.ui.components.PaymentStatusScreen
-import org.cashu.wallet.ui.components.PrimaryButton
 import org.cashu.wallet.ui.components.QrCard
 import org.cashu.wallet.ui.components.SecondaryButton
 import org.cashu.wallet.ui.components.SectionHeader
@@ -488,7 +486,6 @@ private fun CashuRequestAmountEditSheet(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .navigationBarsPadding()
                 .imePadding()
                 .padding(horizontal = CashuTheme.spacing.comfortable),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -509,13 +506,13 @@ private fun CashuRequestAmountEditSheet(
                 formatter = formatter,
             )
             Spacer(Modifier.weight(1f))
-            NumberPad(amount = amount, onAmountChange = { amount = it }, decimals = decimals)
-            Spacer(Modifier.height(CashuTheme.spacing.snug))
-            PrimaryButton(
-                text = "Done",
-                onClick = { onDone(UnitAmountEntry.baseUnits(amount, decimals).takeIf { it > 0 }) },
+            NumberPadFooter(
+                amount = amount,
+                onAmountChange = { amount = it },
+                decimals = decimals,
+                buttonText = "Done",
+                onButtonClick = { onDone(UnitAmountEntry.baseUnits(amount, decimals).takeIf { it > 0 }) },
             )
-            Spacer(Modifier.height(CashuTheme.spacing.snug))
         }
     }
 }

android/app/src/main/java/org/cashu/wallet/ui/receive/ReceiveLightningScreen.kt
@@ -86,7 +86,7 @@ import org.cashu.wallet.ui.components.InlineNotice
 import org.cashu.wallet.ui.components.InspectorRow
 import org.cashu.wallet.ui.components.MintAvatar
 import org.cashu.wallet.ui.components.MintPickerSheet
-import org.cashu.wallet.ui.components.NumberPad
+import org.cashu.wallet.ui.components.NumberPadFooter
 import org.cashu.wallet.ui.components.PaymentStatusPhase
 import org.cashu.wallet.ui.components.PaymentStatusScreen
 import org.cashu.wallet.ui.components.PrimaryButton
@@ -448,15 +448,15 @@ private fun InputFace(
             InlineNotice(text = errorText)
         }
         Spacer(Modifier.weight(1f))
-        NumberPad(amount = amount, onAmountChange = onAmountChange, decimals = decimals)
-        Spacer(Modifier.height(CashuTheme.spacing.page))
-        PrimaryButton(
-            text = if (creating) "Creating…" else selectedMethod.createActionTitle,
-            onClick = onCreate,
-            enabled = !creating,
-            loading = creating,
+        NumberPadFooter(
+            amount = amount,
+            onAmountChange = onAmountChange,
+            decimals = decimals,
+            buttonText = if (creating) "Creating…" else selectedMethod.createActionTitle,
+            onButtonClick = onCreate,
+            buttonEnabled = !creating,
+            buttonLoading = creating,
         )
-        Spacer(Modifier.navigationBarsPadding())
     }
 }
 

android/app/src/main/java/org/cashu/wallet/ui/send/SendEcashScreen.kt
@@ -81,7 +81,7 @@ import org.cashu.wallet.ui.components.CashuTextField
 import org.cashu.wallet.ui.components.InlineNotice
 import org.cashu.wallet.ui.components.MintPickerSheet
 import org.cashu.wallet.ui.components.MintSelectorRow
-import org.cashu.wallet.ui.components.NumberPad
+import org.cashu.wallet.ui.components.NumberPadFooter
 import org.cashu.wallet.ui.components.PrimaryButton
 import org.cashu.wallet.ui.components.QrCard
 import org.cashu.wallet.ui.components.SheetHeader
@@ -515,17 +515,15 @@ private fun InputFace(
 
         Spacer(modifier = Modifier.weight(1f, fill = true))
 
-        NumberPad(amount = amount, onAmountChange = onAmountChange, decimals = decimals)
-
-        Spacer(Modifier.height(CashuTheme.spacing.page))
-        PrimaryButton(
-            text = if (sending) "Sending…" else "Send",
-            onClick = onSend,
-            enabled = canSend,
-            loading = sending,
+        NumberPadFooter(
+            amount = amount,
+            onAmountChange = onAmountChange,
+            decimals = decimals,
+            buttonText = if (sending) "Sending…" else "Send",
+            onButtonClick = onSend,
+            buttonEnabled = canSend,
+            buttonLoading = sending,
         )
-        // Idiomatic inset spacer: exactly the navigation-bar height at the bottom.
-        Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.navigationBars))
     }
 }
 

android/app/src/main/java/org/cashu/wallet/ui/send/UnifiedSendScreen.kt
@@ -83,7 +83,7 @@ import org.cashu.wallet.ui.components.InspectorRow
 import org.cashu.wallet.ui.components.MintPickerSheet
 import org.cashu.wallet.ui.components.MintSelectorRow
 import org.cashu.wallet.ui.components.NoticeSeverity
-import org.cashu.wallet.ui.components.NumberPad
+import org.cashu.wallet.ui.components.NumberPadFooter
 import org.cashu.wallet.ui.components.PaymentStatusPhase
 import org.cashu.wallet.ui.components.PaymentStatusScreen
 import org.cashu.wallet.ui.components.PrimaryButton
@@ -782,14 +782,13 @@ private fun AmountFace(
             )
         }
         Spacer(Modifier.weight(1f))
-        NumberPad(amount = amount, onAmountChange = onAmountChange)
-        Spacer(Modifier.height(CashuTheme.spacing.page))
-        PrimaryButton(
-            text = "Continue",
-            onClick = onContinue,
-            enabled = amountValue > 0,
+        NumberPadFooter(
+            amount = amount,
+            onAmountChange = onAmountChange,
+            buttonText = "Continue",
+            onButtonClick = onContinue,
+            buttonEnabled = amountValue > 0,
         )
-        Spacer(Modifier.navigationBarsPadding())
     }
 }
 

cashubtc/wallet

android/app/src/main/java/com/cashu/me/ui/components/NumberPad.kt
@@ -10,8 +10,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Backspace
 import androidx.compose.material3.Icon
@@ -29,6 +33,7 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.cashu.me.Core.UnitAmountEntry
+import com.cashu.me.ui.theme.CashuTheme
 
 // Minimal keypad: no background boxes, just numbers with subtle press feedback.
 // Tighter vertical spacing for a cleaner iOS-style appearance.
@@ -100,6 +105,36 @@ fun NumberPad(
                 }
             }
         }
+    }
+}
+
+/**
+ * Shared tail for every amount-entry screen: [NumberPad] followed by a primary
+ * action button, spaced with the app-wide keypad→button gap (`spacing.page`)
+ * and a bottom spacer sized to the real navigation-bar/gesture-bar inset.
+ * Centralizing this stops each screen from hand-deriving its own spacing.
+ */
+@Composable
+fun NumberPadFooter(
+    amount: String,
+    onAmountChange: (String) -> Unit,
+    buttonText: String,
+    onButtonClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    decimals: Int = 0,
+    buttonEnabled: Boolean = true,
+    buttonLoading: Boolean = false,
+) {
+    Column(modifier = modifier) {
+        NumberPad(amount = amount, onAmountChange = onAmountChange, decimals = decimals)
+        Spacer(Modifier.height(CashuTheme.spacing.page))
+        PrimaryButton(
+            text = buttonText,
+            onClick = onButtonClick,
+            enabled = buttonEnabled,
+            loading = buttonLoading,
+        )
+        Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.navigationBars))
     }
 }
 

android/app/src/main/java/com/cashu/me/ui/receive/CashuRequestDetailScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -81,10 +80,9 @@ import com.cashu.me.ui.components.GhostButton
 import com.cashu.me.ui.components.InlineNoticeHost
 import com.cashu.me.ui.components.InspectorRow
 import com.cashu.me.ui.components.MintPickerSheet
-import com.cashu.me.ui.components.NumberPad
+import com.cashu.me.ui.components.NumberPadFooter
 import com.cashu.me.ui.components.PaymentStatusPhase
 import com.cashu.me.ui.components.PaymentStatusScreen
-import com.cashu.me.ui.components.PrimaryButton
 import com.cashu.me.ui.components.QrCard
 import com.cashu.me.ui.components.SecondaryButton
 import com.cashu.me.ui.components.SectionHeader
@@ -488,7 +486,6 @@ private fun CashuRequestAmountEditSheet(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .navigationBarsPadding()
                 .imePadding()
                 .padding(horizontal = CashuTheme.spacing.comfortable),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -509,13 +506,13 @@ private fun CashuRequestAmountEditSheet(
                 formatter = formatter,
             )
             Spacer(Modifier.weight(1f))
-            NumberPad(amount = amount, onAmountChange = { amount = it }, decimals = decimals)
-            Spacer(Modifier.height(CashuTheme.spacing.snug))
-            PrimaryButton(
-                text = "Done",
-                onClick = { onDone(UnitAmountEntry.baseUnits(amount, decimals).takeIf { it > 0 }) },
+            NumberPadFooter(
+                amount = amount,
+                onAmountChange = { amount = it },
+                decimals = decimals,
+                buttonText = "Done",
+                onButtonClick = { onDone(UnitAmountEntry.baseUnits(amount, decimals).takeIf { it > 0 }) },
             )
-            Spacer(Modifier.height(CashuTheme.spacing.snug))
         }
     }
 }

android/app/src/main/java/com/cashu/me/ui/receive/ReceiveLightningScreen.kt
@@ -86,7 +86,7 @@ import com.cashu.me.ui.components.InlineNotice
 import com.cashu.me.ui.components.InspectorRow
 import com.cashu.me.ui.components.MintAvatar
 import com.cashu.me.ui.components.MintPickerSheet
-import com.cashu.me.ui.components.NumberPad
+import com.cashu.me.ui.components.NumberPadFooter
 import com.cashu.me.ui.components.PaymentStatusPhase
 import com.cashu.me.ui.components.PaymentStatusScreen
 import com.cashu.me.ui.components.PrimaryButton
@@ -448,15 +448,15 @@ private fun InputFace(
             InlineNotice(text = errorText)
         }
         Spacer(Modifier.weight(1f))
-        NumberPad(amount = amount, onAmountChange = onAmountChange, decimals = decimals)
-        Spacer(Modifier.height(CashuTheme.spacing.page))
-        PrimaryButton(
-            text = if (creating) "Creating…" else selectedMethod.createActionTitle,
-            onClick = onCreate,
-            enabled = !creating,
-            loading = creating,
+        NumberPadFooter(
+            amount = amount,
+            onAmountChange = onAmountChange,
+            decimals = decimals,
+            buttonText = if (creating) "Creating…" else selectedMethod.createActionTitle,
+            onButtonClick = onCreate,
+            buttonEnabled = !creating,
+            buttonLoading = creating,
         )
-        Spacer(Modifier.navigationBarsPadding())
     }
 }
 

android/app/src/main/java/com/cashu/me/ui/send/SendEcashScreen.kt
@@ -81,7 +81,7 @@ import com.cashu.me.ui.components.CashuTextField
 import com.cashu.me.ui.components.InlineNotice
 import com.cashu.me.ui.components.MintPickerSheet
 import com.cashu.me.ui.components.MintSelectorRow
-import com.cashu.me.ui.components.NumberPad
+import com.cashu.me.ui.components.NumberPadFooter
 import com.cashu.me.ui.components.PrimaryButton
 import com.cashu.me.ui.components.QrCard
 import com.cashu.me.ui.components.SheetHeader
@@ -515,17 +515,15 @@ private fun InputFace(
 
         Spacer(modifier = Modifier.weight(1f, fill = true))
 
-        NumberPad(amount = amount, onAmountChange = onAmountChange, decimals = decimals)
-
-        Spacer(Modifier.height(CashuTheme.spacing.page))
-        PrimaryButton(
-            text = if (sending) "Sending…" else "Send",
-            onClick = onSend,
-            enabled = canSend,
-            loading = sending,
+        NumberPadFooter(
+            amount = amount,
+            onAmountChange = onAmountChange,
+            decimals = decimals,
+            buttonText = if (sending) "Sending…" else "Send",
+            onButtonClick = onSend,
+            buttonEnabled = canSend,
+            buttonLoading = sending,
         )
-        // Idiomatic inset spacer: exactly the navigation-bar height at the bottom.
-        Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.navigationBars))
     }
 }
 

android/app/src/main/java/com/cashu/me/ui/send/UnifiedSendScreen.kt
@@ -83,7 +83,7 @@ import com.cashu.me.ui.components.InspectorRow
 import com.cashu.me.ui.components.MintPickerSheet
 import com.cashu.me.ui.components.MintSelectorRow
 import com.cashu.me.ui.components.NoticeSeverity
-import com.cashu.me.ui.components.NumberPad
+import com.cashu.me.ui.components.NumberPadFooter
 import com.cashu.me.ui.components.PaymentStatusPhase
 import com.cashu.me.ui.components.PaymentStatusScreen
 import com.cashu.me.ui.components.PrimaryButton
@@ -782,14 +782,13 @@ private fun AmountFace(
             )
         }
         Spacer(Modifier.weight(1f))
-        NumberPad(amount = amount, onAmountChange = onAmountChange)
-        Spacer(Modifier.height(CashuTheme.spacing.page))
-        PrimaryButton(
-            text = "Continue",
-            onClick = onContinue,
-            enabled = amountValue > 0,
+        NumberPadFooter(
+            amount = amount,
+            onAmountChange = onAmountChange,
+            buttonText = "Continue",
+            onButtonClick = onContinue,
+            buttonEnabled = amountValue > 0,
         )
-        Spacer(Modifier.navigationBarsPadding())
     }
 }
 